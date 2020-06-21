6880075e7fb5628012a40395ef30dc966a4048ee
Matheus Paice <matheus.paice@ikatec.com.br>
2020-06-21 12:12:58 -0300

    fix:adiciona graficos

--- src/pages/Panel/Ranges/Ranges.tsx
+++ src/pages/Panel/Ranges/Ranges.tsx
@@ -1,9 +1,37 @@
 import React from "react";
 
+import { PieChart, Pie, Sector, Cell, Legend } from "recharts";
+
+import "./style.css";
+
 interface Props {}
 
+const data = [
+  { name: "Clientes Inadimplentes", value: 400 },
+  { name: "Clientes Adimplentes", value: 350 },
+  { name: "Total arrecadado", value: 250 }
+];
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
+
 const Ranges: React.FC<Props> = (props) => {
-  return <div>ranges</div>;
+  return (
+    <div className="container-ranges">
+      <span className="totals"> Total: 150 </span>
+      <PieChart width={500} height={500}>
+        <Pie
+          data={data}
+          label
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Legend verticalAlign="bottom" height={36} />
+      </PieChart>
+    </div>
+  );
 };
 
 export default Ranges;
